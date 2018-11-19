ab89204786648179eee69c46503b5cbca5658846
Morgan Costigan <morgan.costigan@gmail.com>
2018-11-19 16:50:59 -0600

    GET and POST working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,42 @@
 $( document ).ready( readyNow );
 
+function addSong() {
+    //get user inputs
+    //package in an object
+    //send to server via AJAX
+    const objectToSend = {
+        artist: $('#artistIn').val(),
+        track: $('#trackIn').val(),
+        published: $('#publishedIn').val(),
+        rank: $('#rankIn').val(),
+    }//end objectToSend
+    console.log('sending', objectToSend);
+    $.ajax({
+        method: 'POST',
+        url: '/songs',
+        data: objectToSend
+    }).then( function (response) {
+        console.log('back from POST with:', response);
+    }).catch( function (err) {
+        console.log('error retrieving data:', err);
+    })
+    getSongs();
+}// end addSong function
+
+function getSongs() {
+    $.ajax({
+        method: 'GET',
+        url: '/songs'
+    }).then( function (response) {
+        console.log('back from GET with:', response);
+        
+    }).catch( function (err) {
+        console.log('error GETTING:', err);
+    })
+}//end getSongs function
+
 function readyNow(){
     console.log( 'JQ' );
+    $('#addSongButton').on('click', addSong)
+    getSongs();
 } // end readynow
--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.listen( port, ( req, res )=>{
 });
 
 //test route  //////////////////////////////
-app.get('/test', (req, res) => {  //////////////////////////////
+app.get('/songs', (req, res) => {  //////////////////////////////
     console.log('/test GET hit');  //////////////////////////////
     //create a query
     const queryString = `SELECT * FROM songs;`;
@@ -47,3 +47,16 @@ app.get('/test', (req, res) => {  //////////////////////////////
     })
     // res.send('grrrr');  //////////////////////////////
 })//end test GET  //////////////////////////////
+
+app.post('/songs', (req, res) =>{
+    console.log('in /songs POST:', req.body);
+    //create query string
+    const queryString = `INSERT INTO songs (artist, track, rank, published)
+    VALUES ($1, $2, $3, $4);`;
+    pool.query(queryString, [req.body.artist, req.body.track, req.body.rank, req.body.published,]).then( ()=>{
+        res.sendStatus(201);
+    }).catch( (err) =>{
+        console.log('error writing song:', err);
+        res.sendStatus(500);
+    })//end query    
+})//end POST
